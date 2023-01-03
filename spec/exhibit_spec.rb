7db514ae4590bf969085c914283059a712013fa4
Bobby Luly <bobbyluly@gmail.com>
2023-01-03 09:15:50 -0700

    Test add patron tests

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,11 +1,12 @@
 require 'rspec'
 require './lib/exhibit'
-# require './lib/patron'
+require './lib/patron'
 
 RSpec.describe Exhibit do
   let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+  let(:patron_1) { Patron.new("Bob", 20) }
 
-  describe "#initialize" do
+  describe "#initialize exhibit" do
     it "exists" do
       expect(exhibit).to be_an_instance_of(Exhibit)
     end
@@ -13,6 +14,17 @@ RSpec.describe Exhibit do
     it "has attributes" do
       expect(exhibit.name).to eq("Gems and Minerals")
       expect(exhibit.cost).to eq(0)
+    end
+  end
+
+  describe "#initialize patron" do
+    it "exists" do
+      expect(patron_1).to be_an_instance_of(Patron)
+    end
+
+    it "has attributes" do
+      expect(patron_1.name).to eq("Bob")
+      expect(patron_1.spending_money).to eq(20)
     end
   end
 
