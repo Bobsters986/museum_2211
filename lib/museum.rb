155eb5a6f9eed9054d1dfd7af2357fec338ef716
Bobby Luly <bobbyluly@gmail.com>
2023-01-03 10:10:07 -0700

    Test&Feat- Add recommended_exhibits test and method to pass

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,13 @@ class Museum
   def add_exhibit(new_exhibit)
     @exhibits << new_exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommended_array =  patron.interests.map do |interest|
+      exhibits.select do |exhibit|
+        exhibit.name == interest
+      end
+    end.flatten!
+
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Museum do
   let(:gems_and_minerals) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
   let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
   let(:imax) { Exhibit.new({name: "IMAX", cost: 15}) }
+  let(:patron_1) { Patron.new("Bob", 20) }
+  let(:patron_2) { Patron.new("Sally", 20) }
 
   describe "#initialize museum" do
     it "exists" do
@@ -27,6 +29,21 @@ RSpec.describe Museum do
       dmns.add_exhibit(dead_sea_scrolls)
       dmns.add_exhibit(imax)
       expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end
+
+  describe "#recommend_exhibits" do
+    it "can recommend exhibits based on patron interests" do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("IMAX")
+
+      expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scrolls])
+      expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
     end
   end
 
